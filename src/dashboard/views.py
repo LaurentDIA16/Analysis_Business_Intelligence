45dc45502f1be273d184d4cd1d6c41526d738606
LaurentDIA16 <dia16@polenumerique.re>
2022-12-11 19:45:28 +0400

    Création views dashboard cards

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -2,10 +2,22 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from dashboard.forms import InputFileForm
 from dashboard.functions.functions import handle_uploaded_file   
+from .models import *
+from django.db.models import FloatField, Sum
+from django.db.models.functions import Cast
 
 # Create your views here.
 def home(request):
-    return render(request,"dashboard/home.html")
+    invoices = Invoice.objects.all().count()
+    products = Product.objects.all().count()
+    countries = Country.objects.all().count()
+
+    earningsfloat = DetailInvoice.objects.annotate(as_float=Cast('totalcost', FloatField())).aggregate(Sum('as_float'))
+    earnings = int(earningsfloat['as_float__sum'])
+
+    context = {'invoices':invoices,'products':products,'countries':countries,'earnings':earnings}
+
+    return render(request,"dashboard/home.html", context)
 
 def import_csv(request):  
     if request.method == 'POST':  
