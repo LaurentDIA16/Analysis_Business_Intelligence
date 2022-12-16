06b582a7837fb262bb5d760463c927c1872efa66
LaurentDIA16 <dia16@polenumerique.re>
2022-12-16 09:59:09 +0400

    Création urls graphique top & flop

--- src/AnalysisBusinessIntelligence/urls.py
+++ src/AnalysisBusinessIntelligence/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from dashboard.views import home, import_csv, analyseData, cleanData, deleteData
-from dashboard.views import sellByCountryTop, sellByCountryFlop, sellByProductTop, sellByProductFlop, sellByCountryProduct 
+from dashboard.views import sellByCountryTop, sellByCountryTop2010, sellByCountryTop2011, sellByCountryFlop, sellByCountryFlop2010, sellByCountryFlop2011
+from dashboard.views import sellByProductTop, sellByProductTop2010, sellByProductTop2011, sellByProductFlop, sellByProductFlop2010, sellByProductFlop2011
+from dashboard.views import sellByCountryProduct 
 from accounts.views import login_user, logout_user
 
 urlpatterns = [
@@ -14,9 +16,20 @@ urlpatterns = [
     path("connexion/", login_user, name="login"),
     path("déconnexion/", logout_user, name="logout"),
     path("graphique-region/", sellByCountryTop, name="graphique-region"),
-    path("graphique-region/Flop", sellByCountryFlop, name="graphique-region-flop"),
+    path("graphique-region/top/", sellByCountryTop, name="graphique-region-top"),
+    path("graphique-region/top/2010/", sellByCountryTop2010, name="graphique-region-top-2010"),
+    path("graphique-region/top/2011/", sellByCountryTop2011, name="graphique-region-top-2011"),
+    path("graphique-region/flop/", sellByCountryFlop, name="graphique-region-flop"),
+    path("graphique-region/flop/2010/", sellByCountryFlop2010, name="graphique-region-flop-2010"),
+    path("graphique-region/flop/2011/", sellByCountryFlop2011, name="graphique-region-flop-2011"),
+    path("graphique-region/flop/", sellByCountryFlop, name="graphique-region-flop"),
+    path("graphique-produit/top/", sellByProductTop, name="graphique-produit-top"),
     path("graphique-produit/", sellByProductTop, name="graphique-produit"),
-    path("graphique-produit/Flop", sellByProductFlop, name="graphique-produit-flop"),
+    path("graphique-produit/top/2010/", sellByProductTop2010, name="graphique-produit-top-2010"),
+    path("graphique-produit/top/2011/", sellByProductTop2011, name="graphique-produit-top-2011"),
+    path("graphique-produit/flop/", sellByProductFlop, name="graphique-produit-flop"),
+    path("graphique-produit/flop/2010/", sellByProductFlop2010, name="graphique-produit-flop-2010"),
+    path("graphique-produit/flop/2011/", sellByProductFlop2011, name="graphique-produit-flop-2011"),
     path("graphique-region-produit/", sellByCountryProduct, name="graphique-region-produit"),   
 ]
 
