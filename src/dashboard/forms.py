3e75dadd59377b17eccfe1f4be1f131bb3923f9c
LaurentDIA16 <dia16@polenumerique.re>
2022-12-19 12:11:11 +0400

    Update page import CSV

--- src/dashboard/forms.py
+++ src/dashboard/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.db import models
 
+
 class InputFileForm(forms.Form): 
-    file = forms.FileField(label='') # for creating file input  
+    file = forms.FileField(label='', widget=forms.FileInput(attrs={'accept': ".csv"})) # for creating file input  
