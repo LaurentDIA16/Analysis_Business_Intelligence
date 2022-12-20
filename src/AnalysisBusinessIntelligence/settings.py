f2d9a4e39097feea6e740640600b75c71df989b4
LaurentDIA16 <dia16@polenumerique.re>
2022-12-20 18:18:02 +0400

    Update message d'erreur

--- src/AnalysisBusinessIntelligence/settings.py
+++ src/AnalysisBusinessIntelligence/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+from django.contrib.messages import constants as messages
 from pathlib import Path
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,6 +128,15 @@ STATIC_URL = '/static/'
 
 # ************** Ajouter par Laurent ***************
 AUTH_USER_MODEL = "accounts.CustomUser"
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+ }
